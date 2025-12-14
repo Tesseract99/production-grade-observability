ad09a358cd2d840fdc9cac2e8f91f60fb3247ce7
PRP2401 <prithviraj.p@nielsen.com>
2025-12-14 14:43:26 +0530

    fixed docker build script

--- src/docker-build.sh
+++ src/docker-build.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-podman build -t tesseract/myapp-go .
+COMMIT_HASH=$(git rev-parse --short HEAD)
+APP_VERSION=$(cat version.txt)
+
+if [ -z "$COMMIT_HASH" ] || [ -z "$APP_VERSION" ]; then
+    echo "Error: version.txt or git commit hash is empty"
+    exit 1
+fi
+
+echo "Building image tesseract/myapp-go:v$APP_VERSION-$COMMIT_HASH"
+
+podman build -t tesseract/myapp-go:$APP_VERSION-$COMMIT_HASH .
